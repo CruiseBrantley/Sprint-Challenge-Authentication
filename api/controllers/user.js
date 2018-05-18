ad64deafcdd9e707376e7a8291ec4cb6f5df5bb8
Cruise Brantley <cruise.brantley@gmail.com>
2018-05-18 11:52:32 -0500

    register working

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,8 +1,14 @@
-const User = require('../models/userModels');
-const bcrypt = require('bcrypt');
+const User = require("../models/userModels");
+const bcrypt = require("bcrypt");
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
+  const user = new User({ username, password });
+
+  user.save((err, user) => {
+    if (err) return res.send(err);
+    res.send(user);
+  });
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
 };
